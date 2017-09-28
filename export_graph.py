5076f7b2d5a59b2541ae66c6d03686782919859f
Michael Tesař <michael.tesar@nudz.cz>
2017-09-28 19:34:53 +0200

    Prototyping exporting code to graph

--- export_graph.py
+++ export_graph.py
@@ -1,8 +1,31 @@
+"""
+Export trained classifier to graph
+==================================
+Load trained classifier and save the whole model
+as tree graph (true/false) with all features and
+target colors. It assumes that classifier is
+already generated and saved.
+
+Usage:
+    dot -Tpng color_graph.dot -o tree.png
+
+Install dot command line tool:
+    xcode-select --install
+
+Michael Tesar <michtesar@gmail.com>
+28-9-2017 Drahotesice
+"""
+
 import pickle
 from sklearn import tree
-import graphviz
 
 clf = pickle.load(open('color_classifier.sav', 'rb'))
-dot_data = tree.export_graphviz(clf, out_file=None) 
-graph = graphviz.Source(dot_data) 
-graph
+tree.export_graphviz(clf, out_file='color_graph.dot')
+
+"""
+,
+feature_names=iris.feature_names,
+class_names=iris.target_names,
+filled=True, rounded=True,
+special_characters=True
+"""
